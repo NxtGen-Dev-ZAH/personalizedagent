fb629db3ab5c79a3db8e838e01d9798dd90aa457
Zaheer Ahmed <163631973+NxtGen-Dev-ZAH@users.noreply.github.com>
2026-01-20 02:56:40 +0500

    Update database URL placeholder in config.py
    Updated the database URL placeholder in config.py.

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     app_name: str = "Personal AI Platform API"
     environment: Literal["local", "staging", "production"] = "local"
     database_url: str = (
-        'postgresql+asyncpg://neondb_owner:npg_UvnDjz8a1PEX@ep-weathered-bread-a15h3p85-pooler.ap-southeast-1.aws.neon.tech/neondb'
+       #place database url here 
     )
     openai_api_key: str | None = None
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO"
